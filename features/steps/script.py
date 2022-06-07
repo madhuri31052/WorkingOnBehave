5728f8f9fbbc23f37e95f315a607c1c5ce26b437
Madhuri Matta <mleela.madhuri@strongarmtech.com>
2022-06-07 15:54:15 +0530

    added timesleeps

--- features/steps/script.py
+++ features/steps/script.py
@@ -8,18 +8,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 def LaunchChrome(context):
     context.driver=webdriver.Chrome(ChromeDriverManager().install() )
     context.driver.maximize_window()
- 
+    time.sleep(5)
 
 @when('Go to Safework login page')
 def SafewordDashboard(context):
     context.driver.get("https://qa.strongarmtech.com/login")
-   
+    time.sleep(5)
 
 @when('Enters "{email}" and "{pwd}"')
 def EnterCred(context, email, pwd):
     context.driver.find_element_by_id("username").send_keys(email)
     context.driver.find_element_by_id("password").send_keys(pwd)
-   
+    time.sleep(5)
 
 # email = os.environ.get('Email_DB')
 # pwd = os.environ.get('Pass_DB')
@@ -28,12 +28,12 @@ def EnterCred(context, email, pwd):
 def EnterCred(context):
     context.driver.find_element_by_id("username").send_keys("mleela.madhuri@strongarmtech.com")
     context.driver.find_element_by_id("password").send_keys("qaAccess@491")
-  
+    time.sleep(5)
 
 @when('Click on login button')
 def LoginButton(context):
     context.driver.find_element_by_xpath("//span[text()='Login']").click()
- 
+    time.sleep(9)
 
 @then('User should not be able to login')
 def LoginCheck(context):
@@ -47,12 +47,12 @@ def LoginCheck(context):
 @when('Click on Multibox tab')
 def MultiboxTab(context):
     context.driver.find_element_by_xpath("//body/div[@id='root']/div[1]/div[1]/div[3]/div[1]").click()
-
+    time.sleep(5)
 
 @when('Click on Signout tab')
 def SignoutTab(context):
     context.driver.find_element_by_xpath("//p[contains(text(),'Sign out')]").click()
-
+    time.sleep(5)
 
 @then('User should be signed out')
 def Signedout(context):
@@ -63,12 +63,12 @@ def Signedout(context):
 @when('Select Analytics tab')
 def AnalyticsTab(context):
     context.driver.find_element_by_xpath("//body/div[@id='root']/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/a[3]/button[1]/span[1]").click()
-   
+    time.sleep(12)
 
 @when('On Ergonomic Safety Dashboard click on Select Athlete dropdown')
 def select(context):
     context.driver.find_element_by_xpath("//div[@id='Select AthleteSelect']").click()
-
+    time.sleep(5)
 
 @then('User should not be able to Select an Athlete')
 def dropdown(context):
@@ -83,12 +83,12 @@ def dropdown(context):
 @when('Click on Fuse Dashboard tab')
 def fuseTab(context):
     context.driver.find_element_by_xpath("//body/div[@id='root']/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/*[1]").click()
-   
+    time.sleep(8)
 
 @when('Select New Hire Tenure Analysis')
 def hiretenure(context):
     context.driver.find_element_by_xpath("//span[contains(text(),'New Hire Tenure Analysis')]").click()
-    
+    time.sleep(10)
 
 @then('User should be able to see Highest Risk Tenure Group tile')
 def Tile(context):
@@ -103,17 +103,15 @@ def Tile(context):
 @when('Select corporate report tab')
 def reportTab(context):
     context.driver.find_element_by_xpath("//span[contains(text(), 'Corporate Report')]").click()
-   
+    time.sleep(12)
 
 @when('Click on sort icon of Warehouse')
 def warehouse(context):
     context.driver.find_element_by_xpath("//body/div[@id='root']/div[1]/div[2]/div[1]/div[2]/main[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/span[1]/*[1]").click()
-   
+    time.sleep(5)
 
 @then('User should be able to sort')
 def sortIcon(context):
     context.driver.find_element_by_xpath("//body/div[@id='root']/div[1]/div[2]/div[1]/div[2]/main[1]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]").is_displayed()
     assert True
     context.driver.close()
-
-print("Successful")
